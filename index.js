a3c8c1aa51abdb323357673c77caf2c7ff426cd7
Tharun-sheelam <157952192+Tharun-sheelam@users.noreply.github.com>
2025-09-14 16:50:48 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client'; // Updated import
-import App from './App';
-import './index.css';
-
-const root = document.getElementById('root');
-const rootContainer = createRoot(root);
-rootContainer.render(<App />);
